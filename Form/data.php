7fd9581a0630d2ed5d813f7c58569071888b6336
golam-shovon <38292502+golam-shovon@users.noreply.github.com>
2018-06-11 09:24:10 +0600

    Add files via upload

---
+++ Form/data.php
@@ -0,0 +1,8 @@
+<?php
+	$un=$_POST["Username"];
+	$pw=$_POST["Password"];
+	
+	echo "Name: ".$un."<br>";
+	echo "Password: ".$pw."<br>";
+
+?>
---
+++ Form/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+	<Head>	
+		<h1 align=center> Login Form<h2>
+			<form action="data.php" method="post">
+				<table align=center>	
+					<tr>	
+						<td>Username:</td>
+						<td><input type="text" name="Username" placeholder="type username"></td>
+					</tr>
+					<tr>	
+						<td>Password:</td>
+						<td> <input type="password" name="Password"placeholder="password"></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td><button type="button">Cancel</button> <input type="submit" value="Submit"> </td>
+					</tr>
+					
+				</table>
+				<h2 align=center>Or<h2>
+					<p align=center><a href="https://www.facebook.com/login/" target=#><img src="fblogo.svg.png"></a> <a href="https://accounts.google.com/signin/v2/identifier?hl=EN&flowName=GlifWebSignIn&flowEntry=ServiceLogin" target=#><img src="google.svg.png"></a> </p>
+					
+			
+			</form>
+	</head>
+</html>
